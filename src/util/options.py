1de50a274924f401608d5eb0ab096a294afdbfc6
FrGermano <germa.fr@tiscali.it>
2022-12-26 22:18:04 +0100

    definisco meglio funzione di confronto

--- src/util/options.py
+++ src/util/options.py
@@ -1,19 +1,16 @@
-"""Modulo per effettuare parser a riga di comando"""
-
+# Modulo per effettuare parser a riga di comando, formato altamente embrionale e alquanto incompleto
 
 # Standard Import
 import sys
 
 # Site-package Import
 import os
-import shutil
 
 # Project Import
 # from app import application as a
 
 """Creo una classe Request dove l'utente indica il criterio di ricerca
 in base all'estensione dei file presenti in una cartella"""
-
 class Request:
     def __init__(self, ext): 
         self.extension = ext
@@ -22,9 +19,16 @@ class Request:
     def view(self):
         os.getcwd() # ci si posiziona nella directory corrente
         os.chdir("Source") # ci si sposta nella cartella 'Source'
-        print(os.listdir()) # visualizza i file e cartelle nella directory corrente
+        for cartelle, sottocartelle, files in os.walk(os.getcwd()): """ funzione che ottiene la lista delle cartelle, sottocartelle e files 
+                                                                        presenti nel filesystem""" 
+        for file in files: #scorrimento della lista dei files per confronto con estensione richiesta
+            if file.endswith('.txt'): #condizione di confronto estensioni
+                print(file) #stampa lista file con estensione richiesta
+
+        # riga commentata creata per mie esigenze che non deve essere presa in considerazione
+        # print(os.listdir()) # visualizza i file e cartelle nella directory corrente
         
 #creazione dell'istanza della classe e chiamata del metodo view
-req = input("Inserisci il criterio di scelta ")
+req = input("Inserisci l'estensione dei file da ricercare(.ext) ")
 objExt = Request(req)
 objExt.view()
