8f809f624ac91c665c11500c95bc1bafcde5c270
FrGermano <germa.fr@tiscali.it>
2023-01-19 22:24:46 +0100

    piccoli ritocchi su messaggi per imput operatore

--- src/util/options.py
+++ src/util/options.py
@@ -1,11 +1,10 @@
 # Modulo per effettuare parser a riga di comando, formato altamente embrionale e alquanto incompleto
 
 # Standard Import
-import sys
+# import sys
 
 # Site-package Import
 import os
-import re
 
 # Project Import
 # from app import application as a
@@ -19,15 +18,15 @@ class Request:
 
     """definizione di un metodo che visualizza i file presenti nella directory"""
     def view(self):
-        # print(os.listdir("Source"))
         os.chdir("Source") # ci si sposta nella cartella 'Source'
+        """ funzione che ottiene la lista delle cartelle, sottocartelle e files 
+            presenti nel filesystem """
         for cartelle, sottocartelle, files in os.walk(os.getcwd()):
-            # funzione che ottiene la lista delle cartelle, sottocartelle e files 
-            # presenti nel filesystem
-            for file in files: #scorrimento della lista dei files per confronto con estensione richiesta
-                if self.word in file: 
+            """ scorrimento della lista dei files per confronto con estensione richiesta """
+            for file in files:
+                if self.word in file: # se stringa di ricerca coincide con 'file'
                     print(file)
-#creazione dell'istanza della classe e chiamata del metodo view
-req = input("Inserisci l'estensione dei file da ricercare(.ext) ")
+""" creazione dell'istanza della classe e chiamata del metodo view """
+req = input("Inserisci il o i files da ricercare => ")
 objExt = Request(req) #creazione di un istanza della classe Request
 objExt.view() # chiamata del metodo di visualizzazione dei file nel filesystem
