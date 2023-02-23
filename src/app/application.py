06c050cda852395a8c9e35ad49cceefbf11b0188
Luca Zomparelli <ZompaSenior@users.noreply.github.com>
2023-02-23 22:40:08 +0100

    feature/startup (#24)
    * Aggiornati un po' i file durante il laboratorio, da terminare
    
    * Implementati i segnaposto per le restanti parti del progetto.

--- src/app/application.py
+++ src/app/application.py
@@ -5,6 +5,8 @@
 # Site-package Import
 
 # Project Import
+from util import config_manager as cm
+from util import option_manager as om
 
 def main(argv: list) -> int:
     """La funzione main dell'applicazione, si occupa di caricare i componenti e
@@ -16,6 +18,12 @@ def main(argv: list) -> int:
     Returns:
         (int): il codice d'errore da restituire
     """
+    
+    option = om.OptionManager()
+    
+    config = cm.ConfigManager(option)
+    
+    
     
     # TODO: Sostituire con l'apertura dell'interfaccia grafica
     print("Hallo world!")
